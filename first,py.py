cb9964b75149a75c59b22428019ed084018e5d85
nikki432-lab <nikhilgouda432@Gmail.com>
2025-03-25 18:07:27 +0530

    Add files via upload

---
+++ first,py.py
@@ -0,0 +1,13 @@
+print("Good evening..")
+# Variables& Data types
+
+name= "pooja" #string
+age=22 #integer
+height=5.5 #float
+is_present=True #boolean
+print("name of the student is:",name)
+#taking user input
+qualification = input("enter you qualification:")
+print("qualification is:",qualification)
+
+print(is_present)
